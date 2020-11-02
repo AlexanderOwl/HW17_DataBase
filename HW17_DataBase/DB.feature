b6a0e27fb3629c7c7e1a4df3946f9fa1004d9bce
Олександр Owl <aischenko99@gmail.com>
2020-11-02 02:42:29 +0200

    Добавьте файлы проекта.

---
+++ HW17_DataBase/DB.feature
@@ -0,0 +1,39 @@
+﻿Feature: DataBase
+	In order to speed up testing
+	As a QA automation
+	I want the database to be checked automatically
+
+	@create @person
+Scenario: Create new person
+	Given person data TestName, TestLastName, 100, TestCity	
+	When send query to data base
+	Then new user created with person data
+
+	@update @person
+Scenario: Update person data
+	Given update person data TestNameUpdate, TestLastNameUpdate, 12, TestCityUpdate
+	When send query to data base
+	Then new user created with person data
+
+	@create @order
+Scenario: Create order to last person
+	Given order sum 9123
+	When send query to data base
+	Then new order created
+
+	@delete @order
+Scenario: Delete order to last person
+	Given query to delete orders
+	When send query to data base
+	Then order is deleted
+
+	@person
+Scenario: Person id is unique
+	Given select all person id
+	Then all person id is unique
+
+	@delete @person
+Scenario: Delete last person
+	Given query to delete person
+	When send query to data base
+	Then person is deleted
---
+++ HW17_DataBase/DataBaseSteps.cs
@@ -0,0 +1,162 @@
+﻿using NUnit.Framework;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using TechTalk.SpecFlow;
+
+namespace HW17_DataBase
+{
+    [Binding]
+    public class DataBaseSteps
+    {
+        string connectionString = @"Data Source=ALEX-PC;Initial Catalog=TEST_DB;Integrated Security=True";
+        string sqlExpression;
+        SqlDataAdapter adapter;
+
+        SqlConnection connection;
+        SqlCommand sqlCommand;
+        int number;
+
+        string firstName;
+        string lastName;
+        int age;
+        string city;
+
+        #region Create person
+        [Given(@"person data (.*), (.*), (.*), (.*)")]
+        public void GivenPersonData(string firstName, string lastName, int age, string city)
+        {
+            sqlExpression = String.Format("INSERT INTO Persons (FirstName, LastName, Age, City) VALUES ('{0}', '{1}', {2}, '{3}')",
+                 firstName, lastName, age, city);
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.city = city;
+        }
+        [When(@"send query to data base")]
+        public void WhenSendQueryWithDataToDataBase()
+        {
+            using (connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                sqlCommand = new SqlCommand(sqlExpression, connection);
+                number = sqlCommand.ExecuteNonQuery();
+            }
+        }
+        [Then(@"new user created with person data")]
+        public void ThenNewUserCreatedWithPersonData()
+        {
+            sqlExpression = "SELECT * FROM Persons WHERE ID = (SELECT MAX(ID) FROM Persons)";
+            DataSet dataset = GetDataSet();
+            Assert.AreEqual(firstName, dataset.Tables[0].Rows[0].ItemArray[1]);
+            Assert.AreEqual(lastName, dataset.Tables[0].Rows[0].ItemArray[2]);
+            Assert.AreEqual(age, dataset.Tables[0].Rows[0].ItemArray[3]);
+            Assert.AreEqual(city, dataset.Tables[0].Rows[0].ItemArray[4]);
+        }
+        #endregion
+
+        #region Update person
+        [Given(@"update person data (.*), (.*), (.*), (.*)")]
+        public void GivenUpdatePersonData(string firstName, string lastName, int age, string city)
+        {
+            sqlExpression = String.Format("UPDATE Persons SET FirstName='{0}', LastName='{1}', Age='{2}', City='{3}'  WHERE ID=(SELECT MAX(ID) FROM Persons)",
+                  firstName, lastName, age, city);
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.city = city;
+        }
+        #endregion
+
+        #region Create Order
+        int sum;
+        [Given(@"order sum (.*)")]
+        public void GivenOrderSum(int sum)
+        {
+            sqlExpression = String.Format("INSERT INTO Orders(SUM_order, ID) VALUES({0}, (SELECT MAX(ID) FROM Persons))", sum);
+            this.sum = sum;
+        }
+
+        [Then(@"new order created")]
+        public void ThenNewOrderCreated()
+        {
+            sqlExpression = "SELECT * FROM Orders WHERE ID = (SELECT MAX(ID) FROM Persons)";
+            Assert.AreEqual(sum, GetDataSet().Tables[0].Rows[0].ItemArray[1]);
+        }
+        #endregion
+
+        #region Delete orders
+        [Given(@"query to delete orders")]
+        public void GivenDeleteQueryToOrders()
+        {
+            sqlExpression = String.Format("DELETE FROM Orders WHERE ID=(SELECT MAX(ID) FROM Orders)");
+        }
+
+        [Then(@"order is deleted")]
+        public void ThenOrderIsDeleted()
+        {
+            sqlExpression = String.Format("SELECT COUNT(ID) AS Count_ID FROM Orders WHERE ID = (SELECT MAX(ID)+1 FROM Orders)");
+            Assert.AreEqual(0, GetDataSet().Tables[0].Rows[0].ItemArray[0]);
+        }
+
+        public DataSet GetDataSet()
+        {
+            DataSet dataset;
+            using (connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                adapter = new SqlDataAdapter(sqlExpression, connection);
+
+                dataset = new DataSet();
+                adapter.Fill(dataset);
+            }
+            return dataset;
+        }
+        #endregion
+
+        #region Unique person id
+        [Given(@"select all person id")]
+        public void GivenSelectAllPersonId()
+        {
+            sqlExpression = String.Format("SELECT ID FROM Persons");
+        }
+
+        [Then(@"all person id is unique")]
+        public void ThenAllPersonIdIsUnique()
+        {
+            DataSet dataset = GetDataSet();
+            bool flag = false;
+            for (int i = 1; i < dataset.Tables[0].Rows.Count; i++)
+            {
+                if (dataset.Tables[0].Rows[i].ItemArray[0].Equals(dataset.Tables[0].Rows[i - 1].ItemArray[0]))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            Assert.IsFalse(flag);
+        }
+        #endregion
+
+        #region Delete person
+        int maxId;
+        [Given(@"query to delete person")]
+        public void GivenQueryToDeletePerson()
+        {
+            sqlExpression = String.Format("SELECT MAX(ID) AS max_id FROM Persons");
+            maxId = int.Parse(GetDataSet().Tables[0].Rows[0].ItemArray[0].ToString());
+            sqlExpression = String.Format("DELETE FROM Persons WHERE ID = {0}", maxId);
+
+        }
+
+        [Then(@"person is deleted")]
+        public void ThenPersonIsDeleted()
+        {
+            sqlExpression = String.Format("SELECT MAX(ID) AS max_id FROM Persons");
+            int newMaxId = int.Parse(GetDataSet().Tables[0].Rows[0].ItemArray[0].ToString());
+            Assert.AreNotEqual(maxId, newMaxId);
+        }
+        #endregion
+
+    }
+}
